015a6b44d08e6e6cfe6a5a74ae411bdc4f4c39f8
seth <seth.sevier@techngs.com>
2019-05-09 21:45:29 -0400

    Now organized as a 2 function library: text -> model data, model data -> chain

--- marko.py
+++ marko.py
@@ -5,52 +5,56 @@ import random
 import re
 import sys
 
-wordlist = {}
+
+def text_to_training_data(filename):
+    # read in the file, remove special characters
+    buffer = ''
+    with open(filename) as file:
+        for line in file:
+            for char in line:
+                if re.match("[\w\s.]", char) is not None:
+                    if char != '\n':
+                        buffer += char
+                    else:
+                        buffer += ' '
+    words = buffer.split()
+
+    # strip out blank entries
+    for i, word in enumerate(words):
+        if word == '':
+            del words[i]
+    words = [x.lower() for x in words]
+
+    wordlist = {}
+    for i, word in enumerate(words):
+        try:
+            if word not in wordlist:
+                wordlist[word] = [words[i + 1]]
+            else:
+                wordlist[word] += [words[i + 1]]
+        except BaseException:
+            pass
+
+    return wordlist
 
 
-def build_markov_chain(word):
+def build_markov_chain(word, wordlist):
     sys.stdout.write(word + ' ')
     word = word.lower()
     new = random.choice(wordlist[word])
     if word[-1] != '.':
-        build_markov_chain(new)
+        build_markov_chain(new, wordlist)
     else:
-        print()
-
-
-if len(sys.argv) == 1:
-    if os.path.isfile('training_data.txt'):
-        filename = 'training_data.txt'
-    else:
-        print('Error: No training data found!')
-        sys.exit()
-else:
-    filename = sys.argv[1]
-
+        print('')
 # read in file and format it
-buffer = ''
-with open(filename) as file:
-    for line in file:
-        for char in line:
-            if re.match("[\w\s.]", char) is not None:
-                if char != '\n':
-                    buffer += char
-                else:
-                    buffer += ' '
-words = buffer.split()
-for i, word in enumerate(words):
-    if word == '':
-        del words[i]
-words = [x.lower() for x in words]
+
 
 # use list of words for training
-for i, word in enumerate(words):
-    try:
-        if word not in wordlist:
-            wordlist[word] = [words[i + 1]]
-        else:
-            wordlist[word] += [words[i + 1]]
-    except BaseException:
-        pass
 
-build_markov_chain(random.choice(list(wordlist.keys())).title())
+def main():
+    wordlist = text_to_training_data('training_data.txt')
+    build_markov_chain(random.choice(list(wordlist.keys())).title(), wordlist)
+
+
+if __name__ == "__main__":
+    main()
